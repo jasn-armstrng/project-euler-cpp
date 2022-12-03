479b7bc5cbb59133dfc9863564d3dfc06a23299a
Jason Armstrong <jasnarmstrng@gmail.com>
2022-12-03 23:35:43 +0000

    Create new file largest_palindrome_product.cpp

---
+++ project-euler/04-largest-palindrome-product/src/largest_palindrome_product.cpp
@@ -0,0 +1,15 @@
+// This program find the largest palindrome product of two 3-digit numbers
+
+// First approach: mult each element of 100 ... 999 * each in 999 ... 100
+// check if each product is palindrome
+// store the first palindrome. replace the first palindrome with a subsequent
+// higher and so on
+
+// What I need: a function to reverse an int, a function to compare magnitude
+// of previous to the current palindrome
+#include <iostream>
+
+int main() {
+
+  return 0;
+}
